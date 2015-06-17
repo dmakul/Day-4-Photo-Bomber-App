4557e516ccdb3bad3eb85d763cbf8b8d8a1de5e1
Даурен Макул <daurenmakul@MacBook-Pro-Dauren.local>
2015-06-17 12:16:23 +0600

    first commit

---
+++ Day 4 Photo Bombers App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Day 4 Photo Bombers App
+//
+//  Created by Даурен Макул on 17.06.15.
+//  Copyright (c) 2015 Даурен Макул. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Day 4 Photo Bombers App/PhotoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.m
+//  Day 4 Photo Bombers App
+//
+//  Created by Даурен Макул on 17.06.15.
+//  Copyright (c) 2015 Даурен Макул. All rights reserved.
+//
+
+#import "PhotoCollectionViewCell.h"
+
+@implementation PhotoCollectionViewCell
+
+@end
